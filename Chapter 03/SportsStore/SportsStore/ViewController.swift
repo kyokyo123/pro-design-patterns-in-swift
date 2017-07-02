a580e6aa6690e20cbb0f7aa7ccb1233d193f53a9
Hotdog <kyokyo123@gmail.com>
2017-07-03 01:20:50 +0800

    Update to Swift 3 syntax for Chapter 3

--- Chapter 03/SportsStore/SportsStore/ViewController.swift
+++ Chapter 03/SportsStore/SportsStore/ViewController.swift
@@ -7,7 +7,7 @@ class ProductTableCell : UITableViewCell {
     @IBOutlet weak var stockStepper: UIStepper!
     @IBOutlet weak var stockField: UITextField!
     
-    var productId:Int?;
+    var productId:Int?
 }
 
 class ViewController: UIViewController, UITableViewDataSource {
@@ -27,68 +27,68 @@ class ViewController: UIViewController, UITableViewDataSource {
              "Chess", 29.95, 3),
         ("Human Chess Board", "A fun game for the family", "Chess", 75.0, 2),
         ("Bling-Bling King", "Gold-plated, diamond-studded King", 
-             "Chess", 1200.0, 4)];
+             "Chess", 1200.0, 4)]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        displayStockTotal();
+        displayStockTotal() 
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
     }
 
-    func tableView(tableView: UITableView,
+    func tableView(_ tableView: UITableView,
         numberOfRowsInSection section: Int) -> Int {
-        return products.count;
+        return products.count 
     }
     
-    func tableView(tableView: UITableView,
-        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let product = products[indexPath.row];
-        let cell = tableView.dequeueReusableCellWithIdentifier("ProductCell")
-        as ProductTableCell;
-        cell.productId = indexPath.row;
-        cell.nameLabel.text = product.0;
-        cell.descriptionLabel.text = product.1;
-        cell.stockStepper.value = Double(product.4);
-        cell.stockField.text = String(product.4);
-        return cell;
+    func tableView(_ tableView: UITableView,
+        cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let product = products[indexPath.row] 
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ProductCell")
+        as! ProductTableCell 
+        cell.productId = indexPath.row 
+        cell.nameLabel.text = product.0 
+        cell.descriptionLabel.text = product.1 
+        cell.stockStepper.value = Double(product.4) 
+        cell.stockField.text = String(product.4) 
+        return cell 
     }
     
-    @IBAction func stockLevelDidChange(sender: AnyObject) {
+    @IBAction func stockLevelDidChange(_ sender: AnyObject) {
         if var currentCell = sender as? UIView {
             while (true) {
-                currentCell = currentCell.superview!;
+                currentCell = currentCell.superview! 
                 if let cell = currentCell as? ProductTableCell {
-                    if let id = cell.productId? {
+                    if let id = cell.productId {
                     
-                        var newStockLevel:Int?;
+                        var newStockLevel:Int? 
                         
                         if let stepper = sender as? UIStepper {
-                            newStockLevel = Int(stepper.value);
+                            newStockLevel = Int(stepper.value) 
                         } else if let textfield = sender as? UITextField {
-                            if let newValue = textfield.text.toInt()? {
-                                newStockLevel = newValue;
+                            if let newValue = textfield.text {
+                                newStockLevel = Int(newValue)
                             }
                         }
 
                         if let level = newStockLevel {
-                            products[id].4 = level;
-                            cell.stockStepper.value = Double(level);
-                            cell.stockField.text = String(level);
+                            products[id].4 = level 
+                            cell.stockStepper.value = Double(level) 
+                            cell.stockField.text = String(level) 
                         }
                     }
-                    break;
+                    break 
                 }
             }
-            displayStockTotal();
+            displayStockTotal() 
         }
     }
     
     func displayStockTotal() {
         let stockTotal = products.reduce(0,
-        {(total, product) -> Int in return total + product.4});
-        totalStockLabel.text = "\(stockTotal) Products in Stock";
+        {(total, product) -> Int in return total + product.4}) 
+        totalStockLabel.text = "\(stockTotal) Products in Stock" 
     }
 }
--- Chapter 03/Tuples.playground/section-1.swift
+++ Chapter 03/Tuples.playground/section-1.swift
@@ -1,13 +1,13 @@
-import Foundation;
+import Foundation 
 
-var myProduct = ("Kayak", "A boat for one person", "Watersports", 275.0, 10);
+var myProduct = ("Kayak", "A boat for one person", "Watersports", 275.0, 10) 
 
 func writeProductDetails(product: (String, String, String, Double, Int)) {
-    println("Name: \(product.0)");
-    println("Description: \(product.1)");
-    println("Category: \(product.2)");
-    let formattedPrice = NSString(format: "$%.2lf", product.3);
-    println("Price: \(formattedPrice)");
+    print("Name: \(product.0)")
+    print("Description: \(product.1)")
+    print("Category: \(product.2)")
+    let formattedPrice = NSString(format: "$%.2lf", product.3) 
+    print("Price: \(formattedPrice)")
 }
 
-writeProductDetails(myProduct);
+writeProductDetails(product: myProduct) 
