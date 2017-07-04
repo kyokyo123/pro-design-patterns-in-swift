acc36bafa70b5f10583fe1c5fd4d04cc2ddd036f
Hotdog <kyokyo123@gmail.com>
2017-07-05 01:33:28 +0800

    Update to Swift3 syntax for Chapter 5
    Initalization playground

--- Chapter 05/Initialization.playground/section-1.swift
+++ Chapter 05/Initialization.playground/section-1.swift
@@ -1,26 +1,25 @@
 import Foundation
 
 class Sum : NSObject, NSCopying {
-    let resultsCache: [[Int]];
-    var firstValue:Int;
-    var secondValue:Int;
+    var resultsCache: [[Int]]
+    var firstValue:Int 
+    var secondValue:Int 
     
     init(first:Int, second:Int) {
-        resultsCache = [[Int]](count: 10, repeatedValue:
-            [Int](count:10, repeatedValue: 0));
+        resultsCache = [[Int]](repeating:[Int](repeating: 0, count:10), count: 10)
         for i in 0..<10 {
             for j in 0..<10 {
-                resultsCache[i][j] = i + j;
+                resultsCache[i][j] = i + j 
             }
         }
-        self.firstValue = first;
-        self.secondValue = second;
+        self.firstValue = first
+        self.secondValue = second 
     }
     
     private init(first:Int, second:Int, cache:[[Int]]) {
-        self.firstValue = first;
-        self.secondValue = second;
-        resultsCache = cache;
+        self.firstValue = first 
+        self.secondValue = second 
+        resultsCache = cache 
     }
     
     var Result:Int {
@@ -28,21 +27,21 @@ class Sum : NSObject, NSCopying {
             return firstValue < resultsCache.count
                 && secondValue < resultsCache[firstValue].count
                 ? resultsCache[firstValue][secondValue]
-                : firstValue + secondValue;
+                : firstValue + secondValue 
         }
     }
     
-    func copyWithZone(zone: NSZone) -> AnyObject {
+    func copy(with zone: NSZone? = nil) -> Any {
         return Sum(first:self.firstValue,
-            second: self.secondValue,
-            cache: self.resultsCache);
+                   second: self.secondValue,
+                   cache: self.resultsCache)
     }
 }
 
-var prototype = Sum(first:0, second:9);
-var calc1 = prototype.Result;
-var clone = prototype.copy() as Sum;
-clone.firstValue = 3; clone.secondValue = 8;
-var calc2 = clone.Result;
+var prototype = Sum(first:0, second:9) 
+var calc1 = prototype.Result 
+var clone = prototype.copy() as! Sum
+clone.firstValue = 3;  clone.secondValue = 8 
+var calc2 = clone.Result 
 
-println("Calc1: \(calc1) Calc2: \(calc2)");
+print("Calc1: \(calc1) Calc2: \(calc2)")
