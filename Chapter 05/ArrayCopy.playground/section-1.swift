2fd1d012ad96754a6dfccec30eeb91db86eb9547
Hotdog <kyokyo123@gmail.com>
2017-07-12 00:55:38 +0800

    Update to Swift3 syntax for Chapter 5

--- Chapter 05/ArrayCopy.playground/section-1.swift
+++ Chapter 05/ArrayCopy.playground/section-1.swift
@@ -1,31 +1,33 @@
 import Foundation
 
 class Person : NSObject, NSCopying {
-    var name:String;
-    var country:String;
+    var name:String
+    var country:String
     
     init(name:String, country:String) {
-        self.name = name; self.country = country;
+        self.name = name
+        self.country = country
     }
     
-    func copyWithZone(zone: NSZone) -> AnyObject {
-        return Person(name: self.name, country: self.country);
+    func copy(with zone: NSZone? = nil) -> Any {
+        return Person(name: self.name, country: self.country)
+
     }
 }
 
 func deepCopy(data:[AnyObject]) -> [AnyObject] {
     return data.map({item -> AnyObject in
         if (item is NSCopying && item is NSObject) {
-            return (item as NSObject).copy();
+            return (item as! NSObject).copy() as AnyObject
         } else {
-            return item;
+            return item
         }
     })
 }
 
 var people = [Person(name:"Joe", country:"France"),
-    Person(name:"Bob", country:"USA")];
-var otherpeople = deepCopy(people) as [Person];
+    Person(name:"Bob", country:"USA")]
+var otherpeople = deepCopy(data: people) as! [Person]
 
-people[0].country = "UK";
-println("Country: \(otherpeople[0].country)");
+people[0].country = "UK"
+print("Country: \(otherpeople[0].country)")
--- Chapter 05/Hiding.playground/section-1.swift
+++ Chapter 05/Hiding.playground/section-1.swift
@@ -1,63 +1,65 @@
 import Foundation
 
 class Message : NSObject, NSCopying {
-    var to:String;
-    var subject:String;
+    var to:String
+    var subject:String
     
     init(to:String, subject:String) {
-        self.to = to; self.subject = subject;
+        self.to = to
+        self.subject = subject
     }
     
-    func copyWithZone(zone: NSZone) -> AnyObject {
-        return Message(to: self.to, subject: self.subject);
+    func copy(with zone: NSZone? = nil) -> Any {
+        return Message(to: self.to, subject: self.subject)
+        
     }
 }
 
 class DetailedMessage : Message {
-    var from:String;
+    var from:String
     
     init(to: String, subject: String, from:String) {
-        self.from = from;
-        super.init(to: to, subject: subject);
+        self.from = from
+        super.init(to: to, subject: subject)
     }
     
-    override func copyWithZone(zone: NSZone) -> AnyObject {
+    override func copy(with zone: NSZone?) -> Any {
         return DetailedMessage(to: self.to,
-            subject: self.subject, from: self.from);
+                               subject: self.subject, from: self.from)
     }
 }
 
 class MessageLogger {
-    var messages:[Message] = [];
+    var messages:[Message] = []
     
     func logMessage(msg:Message) {
-        messages.append(msg.copy() as Message);
+        messages.append(msg.copy() as! Message)
     }
     
-    func processMessages(callback:Message -> Void) {
+    func processMessages(callback:(Message) -> Void) {
         for msg in messages {
-            callback(msg);
+            callback(msg)
         }
     }
 }
 
-var logger = MessageLogger();
+var logger = MessageLogger()
 
-var message = Message(to: "Joe", subject: "Hello");
-logger.logMessage(message);
+var message = Message(to: "Joe", subject: "Hello")
+logger.logMessage(msg: message)
 
-message.to = "Bob";
-message.subject = "Free for dinner?";
-logger.logMessage(message);
+message.to = "Bob"
+message.subject = "Free for dinner?"
+logger.logMessage(msg: message)
 
-logger.logMessage(DetailedMessage(to: "Alice", subject: "Hi!", from: "Joe"));
+logger.logMessage(msg: DetailedMessage(to: "Alice", subject: "Hi!", from: "Joe"))
 
 
-logger.processMessages({msg -> Void in
+logger.processMessages(callback: {msg -> Void in
     if let detailed = msg as? DetailedMessage {
-        println("Detailed Message - To: \(detailed.to) From: \(detailed.from)"
-            + " Subject: \(detailed.subject)");
+        print("Detailed Message - To: \(detailed.to) From: \(detailed.from)"
+            + " Subject: \(detailed.subject)")
     } else {
-        println("Message - To: \(msg.to) Subject: \(msg.subject)");
+        print("Message - To: \(msg.to) Subject: \(msg.subject)")
     }
-});
+})
